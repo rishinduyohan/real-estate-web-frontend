ebc6c65c7db0e07e3e2a8e03730759bdf40e6109
Rishindu Yohan <rishinduyohan@gmail.com>
2026-03-01 00:55:54 +0530

    feat: update footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LucideAngularModule, Building2, Mail, Phone, MapPin, Facebook, Twitter, Instagram, Linkedin } from 'lucide-angular';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, LucideAngularModule],
+  imports: [CommonModule, LucideAngularModule,RouterModule],
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
@@ -17,4 +18,10 @@ export class FooterComponent {
   readonly Twitter = Twitter;
   readonly Instagram = Instagram;
   readonly Linkedin = Linkedin;
+
+  constructor(private router : Router){}
+
+  onAboutUs():void{
+    this.router.navigate(['/about']);
+  }
 }
